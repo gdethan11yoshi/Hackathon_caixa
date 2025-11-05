c21b92fbd71656a404bbb717307f51f736d300e6
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2025-11-06 00:28:24 +0100

    restructure front files

--- src/front/vite.config.ts
+++ src/front/vite.config.ts
@@ -1,11 +1,21 @@
 import {defineConfig} from 'vite';
+import { resolve } from 'path'
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
-        port: 5173,
-        https: false,
-        strictPort: true,
         allowedHosts: ['.trycloudflare.com']
     },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                admin: resolve(__dirname, 'liveView/index.html'),
+            },
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash][extname]',
+            },
+        },
+    }
 });
